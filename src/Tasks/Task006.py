62ed10d2f873cffd5a7eaee4a533b6fba1b9b758
snehasaraf21 <snehasaraf21@gmail.com>
2025-10-14 17:45:48 +0100

    14 oct updated operator and tasks

--- src/Tasks/Task006.py
+++ src/Tasks/Task006.py
@@ -1,7 +1,6 @@
 #write a prgm to check if a no entered by the user is greater than 100
 
 x= float(input("enter the number"))
-if x > 100:
-    print("x is greater than 100")
-else:
-    print("x is less than 100")
+
+
+print(f"{x} is greater than 100" if x > 100 else f"{x} is less than 100")
