ab710c4158cb97c8825fbec74d0df710e281743d
snehasaraf21 <snehasaraf21@gmail.com>
2025-10-14 17:52:25 +0100

    14 oct updated operator and tasks

--- src/Tasks/Task006.py
+++ src/Tasks/Task006.py
@@ -2,5 +2,4 @@
 
 x= float(input("enter the number"))
 
-
-print(f"{x} is greater than 100" if x > 100 else f"{x} is less than 100")
+print(f"{x} is equal to 100" if x == 100 else f"{x} is greater than 100" if x > 100 else f"{x} is less than 100")
